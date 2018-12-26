a419a749ab7c99229e1638c474032f3e258d3e3f
Ben Greene <benrgreene@gmail.com>
2018-12-26 15:27:48 -0800

    feat: switch to lightbox on product page
    On product pages, use the lightbox for images instead of the zoom

--- core/actions.php
+++ core/actions.php
@@ -16,3 +16,11 @@ add_action( 'wp_enqueue_scripts', 'brg_add_theme_styles' );
 function brg_add_theme_styles() {
   wp_enqueue_style( 'child-theme-styles', get_stylesheet_directory_uri() . '/assets/styles/child.css' );
 }
+
+// Switch out the product zoom for the lightbox
+add_action( 'after_setup_theme', 'brg_product_lightbox', 20 );
+function brg_product_lightbox() {
+  remove_theme_support( 'wc-product-gallery-zoom' );
+  add_theme_support( 'wc-product-gallery-lightbox' );
+  add_theme_support( 'wc-product-gallery-slider' );
+}
